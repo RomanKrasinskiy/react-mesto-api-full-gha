b356ed4006ce16de5948f247d9816c808b795c8e
RomanKrasinskiy <krasinskii_roma@mail.ru>
2023-05-06 13:42:12 +0300

    fixed the display of the user's email in the header when updating the page. Fixed jwt interaction on the client and adding a .env file to the server.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-// const baseURL = 'https://api.mymesto.nomoredomains.monster';
-const baseURL = 'http://localhost:3000';
+const baseURL = 'https://api.mymesto.nomoredomains.monster';
+// const baseURL = 'http://localhost:3000';
 
 
 export const apiConfig = {
